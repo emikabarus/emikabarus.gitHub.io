729c1e2769ccb118b949e1756e4490dd64116502
emikabarus <152234565+emikabarus@users.noreply.github.com>
2024-08-23 16:45:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require 'vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<script type="text/javascript" src="./vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+<link href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+</body>
+</html>
